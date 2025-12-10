99ba33f3fa0cb9d72c79e1729d3c022044f484e4
=cynka02 <=cynka02@interia.pl>
2025-12-10 20:56:15 +0100

    Added input change listener and updated /predict fetch

--- src/app/scripts.js
+++ src/app/scripts.js
@@ -1,32 +1,45 @@
-const form = document.getElementById('predictForm');
-const input = document.getElementById('text');
-const result = document.getElementById('result');
+const form = document.getElementById("predictForm");
+const input = document.getElementById("fileInput");
+const preview = document.getElementById("img_preview");
+const result = document.getElementById("result");
 
 
-/* Handler /predict */
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    /* w przyszłości zmodyfikować do obsługi jpg */
-    const text = input.value
-    try {
-        /* Wysyłanie inputu użytkownika i oczekiwanie na odpowiedź serwera */
-        const resp = await fetch('/predict', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({text})
-        });
+// Miniaturka
+input.addEventListener("change", () => {
+    result.textContent = "";
+    if (input.files.length === 1){
+        const file = input.files[0];
+        const reader = new FileReader();
 
-        /* Obsługa błędu */
-        if (!resp.ok) {
-            const txt = await resp.text();
-            throw new Error(`Błąd serwera: ${resp.status} ${resp.statusText} — ${txt}`);
-        }
+        reader.onload = (e) => {
+            preview.src = e.target.result;
+            preview.style.display = "block"};
 
-        /* Odbiór i wyświetlanie odpowiedzi */
-        const data = await resp.json();
-        input.value = '';
-        result.textContent = data.text_out ?? JSON.stringify(data, null, 2);
-    } catch (err) {
-    result.textContent = '-';
+        reader.readAsDataURL(file);
+    } else{
+        preview.src = "";
+        preview.style.display = "none";
     }
+});
+
+
+// Handler /predict
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", input.files[0]);
+    input.value = "";
+
+    try {
+        // Wysłanie pliku do Pythona
+        const response = await fetch("/predict", {method: "POST", body: formData});
+
+        // Przetwarzanie odpowiedzi
+        const data = await response.json();
+        // Poniższe do zmiany jak zacznie działać model
+        result.textContent = `Nazwa pliku: ${data.filename}\nSzerokość: ${data.size.width}px\nWysokość: ${data.size.height}px`;
+      } catch (err) {
+        result.textContent = err.message;
+      }
 });
